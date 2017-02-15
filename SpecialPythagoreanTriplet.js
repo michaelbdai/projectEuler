f96d124677ee0d7af3fe6b65691a825dcced8c52
michaelbdai <michaelbdai@gmail.com>
2017-02-15 14:52:52 -0800

    first project

---
+++ SpecialPythagoreanTriplet.js
@@ -0,0 +1,16 @@
+/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a^2 + b^2 = c^2
+For example, 32 + 42 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc
+
+*/
+
+// a + b = 1000 - c => a + b < 1000
+// try all a and b pairs until it meet the condition
+
+
+
